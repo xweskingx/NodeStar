2464c3280b97b774f9c9fe82097538e6c1c29db5
Wesley King <kingwe92@gmail.com>
2017-04-04 19:55:04 -0400

    #142665669 - Setup Docker compose to load keycloak configuration

---
+++ keycloak/docker-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ $KEYCLOAK_USER ] && [ $KEYCLOAK_PASSWORD ]; then
+    keycloak/bin/add-user-keycloak.sh --user $KEYCLOAK_USER --password $KEYCLOAK_PASSWORD
+fi
+
+ARGS=$@
+touch /opt/jboss/importlog
+/opt/jboss/keycloak/bin/standalone.sh -Dkeycloak.migration.action=import -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=keycloak-realm-NodeStar.json -Dkeycloak.migration.strategy=OVERWRITE_EXISTING > importlog &
+pid=$!
+
+while sleep 2
+do
+    if fgrep --quiet "Import finished successfully" "importlog"
+    then
+        kill $pid
+        break;
+    fi
+done
+
+exec /opt/jboss/keycloak/bin/standalone.sh $ARGS
+
+exit $?
